5ceb99cd57c4a10cc2a465957d753c6dafc6c4d7
Azam999 <aziskool20@gmail.com>
2023-12-15 23:36:02 -0500

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import ThreeDButton from '../components/ThreeDButton';
 import * as ga from '../lib/ga';
 import GetStarted from '../components/GetStarted';
 
-
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
